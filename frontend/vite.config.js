a57381655c159db069ee82f1226d32f829f4360f
Krishna Mahajan <mahajankrishna2212@gmail.com>
2025-03-13 13:37:01 +0530

    Updated Final TEST 46

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import purgecss from 'vite-plugin-purgecss'
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
@@ -16,9 +15,12 @@ export default defineConfig({
         './index.html',
         './src/**/*.{js,jsx,ts,tsx,html}'
       ],
-      // If you have classes that are dynamically generated,
-      // add them to the safelist array to prevent them from being removed:
-      safelist: ['dynamic-class']
+      safelist: {
+        // Using a regex that matches any class ensures no CSS is removed.
+        // This will whitelist every class from your external libraries (e.g. Bootstrap, Slick Slider)
+        // as well as your own classes.
+        deep: [/^.*$/]
+      }
     })
   ]
 });

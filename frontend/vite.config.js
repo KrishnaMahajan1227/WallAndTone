32a01726a0f4c4e2d1024ddfc61868115c8fc504
Krishna Mahajan <mahajankrishna2212@gmail.com>
2025-03-13 13:38:56 +0530

    Updated Final TEST 47

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,26 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import purgecss from 'vite-plugin-purgecss'
 
+// https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": "http://localhost:8080",
     },
   },
-  plugins: [
-    react(),
-    purgecss({
-      content: [
-        './index.html',
-        './src/**/*.{js,jsx,ts,tsx,html}'
-      ],
-      safelist: {
-        // Using a regex that matches any class ensures no CSS is removed.
-        // This will whitelist every class from your external libraries (e.g. Bootstrap, Slick Slider)
-        // as well as your own classes.
-        deep: [/^.*$/]
-      }
-    })
-  ]
 });

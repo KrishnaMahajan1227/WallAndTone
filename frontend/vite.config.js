45f8e717d51d9f83b2a5451a4610039ab71c6e73
Krishna Mahajan <mahajankrishna2212@gmail.com>
2025-03-13 13:30:31 +0530

    Updated Final TEST 45

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import purgecss from 'vite-plugin-purgecss'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,16 @@ export default defineConfig({
       "/api": "http://localhost:8080",
     },
   },
+  plugins: [
+    react(),
+    purgecss({
+      content: [
+        './index.html',
+        './src/**/*.{js,jsx,ts,tsx,html}'
+      ],
+      // If you have classes that are dynamically generated,
+      // add them to the safelist array to prevent them from being removed:
+      safelist: ['dynamic-class']
+    })
+  ]
 });
